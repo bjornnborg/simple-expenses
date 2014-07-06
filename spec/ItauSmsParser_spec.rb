749169afbcc68372f81cace3f1686271417b3cf9
Bjornn Borg <bjornnborg@gmail.com>
2014-07-06 20:18:38 -0300

    fazendo o parse de transações do cartão do dependente

--- spec/ItauSmsParser_spec.rb
+++ spec/ItauSmsParser_spec.rb
@@ -9,6 +9,13 @@ describe ItauSmsParser do
       linha_simplificada = parser.parse(linha_complexa)
       "06/06/2014;MARIA JOAO;-51,40;".should eq linha_simplificada
     end
+    
+    it "Deve simplificar linhas transação cartão dependente" do
+      linha_complexa = "2014-06-06	00:14:08	in	25001	25001	Compra aprovada no PERSON VISA PLAT p/ REGINA D C SILVA - BFB TAMBORE FARMAIS valor RS 95,26 em 05/06/2014 as 15h59."
+      parser = ItauSmsParser.new
+      linha_simplificada = parser.parse(linha_complexa)
+      "06/06/2014;BFB TAMBORE FARMAIS;-95,26;".should eq linha_simplificada
+    end    
   
   end
 
