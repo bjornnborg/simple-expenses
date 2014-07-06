ede0ba4b5e3e7b6d681b46d5a2703df679ff57ed
Bjornn Borg <bjornnborg@gmail.com>
2014-07-06 18:45:52 -0300

    fazendo o parse de transações do cartão do titular

--- parser/ItauSmsParser.rb
+++ parser/ItauSmsParser.rb
@@ -1,7 +1,17 @@
 class ItauSmsParser
 
   def parse(text_line)
-
+    simple_line = ""
+    debito = text_line[/Compra/].size > 0
+    match = /(\d{4})-(\d{2})-(\d{2}).+?-(.+?)valor.+RS(.+?)em.+/.match(text_line)
+    if (match)
+      simple_line = "#{match[3]}/#{match[2]}/#{match[1]};#{match[4].strip};"
+      if (debito)
+        simple_line += "-"
+      end
+      simple_line += "#{match[5].strip};"
+    end
+    simple_line
   end
 
 end
