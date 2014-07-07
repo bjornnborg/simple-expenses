63ae5bd11a7ecddb359773b73d70642571fa5c83
Bjornn Borg <bjornnborg@gmail.com>
2014-07-07 01:36:43 -0300

    criando método para escrita de conteúdo

--- file/FileUtil.rb
+++ file/FileUtil.rb
@@ -13,8 +13,12 @@ class FileUtil
     conteudo_simplificado  
   end
   
-  def self.write_content(file_name)
-  
+  def self.write_content(file_name, content_lines)
+    File.open(file_name, 'w') do |f|
+      content_lines.each do |line|
+        f.puts line;
+      end
+    end
   end
 
 end
