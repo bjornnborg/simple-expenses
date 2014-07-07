17174f6adc9ada2ab8596d0bee6a13d86b6dc924
Bjornn Borg <bjornnborg@gmail.com>
2014-07-06 23:37:25 -0300

    teste lendo arquivo mais complexo

--- spec/FileUtil_spec.rb
+++ spec/FileUtil_spec.rb
@@ -9,6 +9,13 @@ describe FileUtil do
       content[0].should eq "06/06/2014;RIACHUELO SP TAMB;-43,80;"
     end
     
+    it "Deve ler conteúdo de arquivo complexo" do
+      content = FileUtil.read_content('./spec/resources/complex_file_read.txt')
+      content.size.should be_equal 11
+      content.last.should eq "03/07/2014;SAQUE;-100,00;"
+      content[6].should eq "07/06/2014;ELET.PAULO 819028100;-77,50;"
+    end
+    
     it "Não deve retornar linhas vazias" do
       content = FileUtil.read_content('./spec/resources/contendo_linha_pagamento.txt')
       content.size.should be_equal 2
