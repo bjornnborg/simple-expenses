0163bfe84b8216a8c0cfe04bdb121cb8dc9382d2
Bjornn Borg <bjornnborg@gmail.com>
2014-07-07 01:50:17 -0300

    progredindo no main...

--- Main.rb
+++ Main.rb
@@ -1,1 +1,21 @@
+require './file/FileUtil.rb'
+require './report/ExcelUtil.rb'
 puts "iniciando processamento..."
+puts "hadouken"
+puts /.+/.match("hadouken\n")[0]
+
+file_name = ARGV.first
+conteudo_simplificado = FileUtil.read_content(file_name)
+formulas = ExcelUtil.montar_resumo(conteudo_simplificado, :en)
+#puts formulas
+formulas_array = []
+formulas.each do |k, v|
+  formulas_array << "#{k};#{v}"
+end
+formulas_array << "" << ""
+
+puts formulas_array
+
+FileUtil.write_content(file_name, formulas_array << conteudo_simplificado)
+
+puts "fim do processamento"
