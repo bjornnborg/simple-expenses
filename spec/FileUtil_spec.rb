86a3080ebea90d8b4673607163e87be69ab05cf3
Bjornn Borg <bjornnborg@gmail.com>
2014-07-06 22:17:34 -0300

    ajustando nome do arquivo txt lido nos testes

--- spec/FileUtil_spec.rb
+++ spec/FileUtil_spec.rb
@@ -4,7 +4,7 @@ describe FileUtil do
   context "Gerenciar conteúdo" do
   
     it "Deve ler conteúdo do arquivo" do
-      content = FileUtil.read_content('./spec/resources/sample_file_read.txt')
+      content = FileUtil.read_content('./spec/resources/simple_file_read.txt')
       content.size.should be_equal 2
       content[0].should eq "06/06/2014;RIACHUELO SP TAMB;-43,80;"
     end
