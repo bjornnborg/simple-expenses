46bf033cb4bf208b283634c07a9e24537ac82faf
Bjornn Borg <bjornnborg@gmail.com>
2014-07-06 22:47:31 -0300

    refactoring na montagem da linha resumida

--- parser/ItauSmsParser.rb
+++ parser/ItauSmsParser.rb
@@ -3,20 +3,18 @@ class ItauSmsParser
   def parse(text_line)
     simple_line = ""
     debito = (text_line =~ /compra|pagamento/i)
-    transacao_cartao = /(\d{4})-(\d{2})-(\d{2}).+?-(.+?)valor.+RS(.+?)em.+/.match(text_line)
-    pagamento_titulos = /(\d{4})-(\d{2})-(\d{2}).+?o\spagamento\sde(.+?)foi.+?valor.+R.(.+?)em.+/.match(text_line)
-    if (transacao_cartao)
-      simple_line = "#{transacao_cartao[3]}/#{transacao_cartao[2]}/#{transacao_cartao[1]};#{transacao_cartao[4].strip};"
+    transacao_cartao = /(\d{4})-(\d{2})-(\d{2}).+?-(.+?)valor.+RS(.+?)em.+/
+    pagamento_titulos = /(\d{4})-(\d{2})-(\d{2}).+?o\spagamento\sde(.+?)foi.+?valor.+R.(.+?)em.+/
+    match = transacao_cartao.match(text_line)
+    if (!match)
+      match = pagamento_titulos.match(text_line)
+    end
+    if (match)
+      simple_line = "#{match[3]}/#{match[2]}/#{match[1]};#{match[4].strip};"
       if (debito)
         simple_line += "-"
       end
-      simple_line += "#{transacao_cartao[5].strip};"
-    elsif (pagamento_titulos)
-      simple_line = "#{pagamento_titulos[3]}/#{pagamento_titulos[2]}/#{pagamento_titulos[1]};#{pagamento_titulos[4].strip};"
-      if (debito)
-        simple_line += "-"
-      end
-      simple_line += "#{pagamento_titulos[5].strip};"    
+      simple_line += "#{match[5].strip};"
     end
     simple_line.empty? ? nil : simple_line
   end
