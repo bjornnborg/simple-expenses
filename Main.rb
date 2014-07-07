6be74a71f3dbaf7e7ddc390515db938dca6d32a5
Bjornn Borg <bjornnborg@gmail.com>
2014-07-07 02:05:56 -0300

    Imprimindo detalhes do processamento no main

--- Main.rb
+++ Main.rb
@@ -1,21 +1,20 @@
 require './file/FileUtil.rb'
 require './report/ExcelUtil.rb'
 puts "iniciando processamento..."
-puts "hadouken"
-puts /.+/.match("hadouken\n")[0]
-
 file_name = ARGV.first
+puts "..processando arquivo #{file_name}"
+
 conteudo_simplificado = FileUtil.read_content(file_name)
+puts "..#{conteudo_simplificado.size} transações encontradas"
+
 formulas = ExcelUtil.montar_resumo(conteudo_simplificado, :en)
-#puts formulas
+puts "..#{formulas.size} categorias"
+
 formulas_array = []
 formulas.each do |k, v|
   formulas_array << "#{k};#{v}"
 end
-formulas_array << "" << ""
-
-puts formulas_array
+formulas_array << "" << "" #linhas em branco. deve casar com o excel util
 
 FileUtil.write_content(file_name, formulas_array << conteudo_simplificado)
-
 puts "fim do processamento"
