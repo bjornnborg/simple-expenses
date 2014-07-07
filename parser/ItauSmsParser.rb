6205d446000156e4c43b31a9287e919ffaf786ce
Bjornn Borg <bjornnborg@gmail.com>
2014-07-06 22:44:05 -0300

    reconhecendo transações de pagamentos de boleto

--- parser/ItauSmsParser.rb
+++ parser/ItauSmsParser.rb
@@ -2,14 +2,21 @@ class ItauSmsParser
 
   def parse(text_line)
     simple_line = ""
-    debito = text_line[/Compra/].size > 0
-    match = /(\d{4})-(\d{2})-(\d{2}).+?-(.+?)valor.+RS(.+?)em.+/.match(text_line)
-    if (match)
-      simple_line = "#{match[3]}/#{match[2]}/#{match[1]};#{match[4].strip};"
+    debito = (text_line =~ /compra|pagamento/i)
+    transacao_cartao = /(\d{4})-(\d{2})-(\d{2}).+?-(.+?)valor.+RS(.+?)em.+/.match(text_line)
+    pagamento_titulos = /(\d{4})-(\d{2})-(\d{2}).+?o\spagamento\sde(.+?)foi.+?valor.+R.(.+?)em.+/.match(text_line)
+    if (transacao_cartao)
+      simple_line = "#{transacao_cartao[3]}/#{transacao_cartao[2]}/#{transacao_cartao[1]};#{transacao_cartao[4].strip};"
       if (debito)
         simple_line += "-"
       end
-      simple_line += "#{match[5].strip};"
+      simple_line += "#{transacao_cartao[5].strip};"
+    elsif (pagamento_titulos)
+      simple_line = "#{pagamento_titulos[3]}/#{pagamento_titulos[2]}/#{pagamento_titulos[1]};#{pagamento_titulos[4].strip};"
+      if (debito)
+        simple_line += "-"
+      end
+      simple_line += "#{pagamento_titulos[5].strip};"    
     end
     simple_line.empty? ? nil : simple_line
   end
